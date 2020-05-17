a061889bca86d0efe9e6c625e36ea666f24cf618
danihaidar <31519318+danihaidar@users.noreply.github.com>
2020-05-17 17:58:58 -0300

    fixed all_index appending

--- MLP_incremental_sampling.py
+++ MLP_incremental_sampling.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python
 # coding:
 ###
@@ -8,6 +9,7 @@ import pandas as pd
 import sklearn
 from sklearn import preprocessing
 import glob
+from datetime import datetime
 
 # Model Imports
 from sklearn.neural_network import MLPRegressor
@@ -35,7 +37,7 @@ def loadCsv(filename):
 
 ### Construct data frame
 def data(filename, scale):
-    
+
     #Load data from file
     dataset = np.array(loadCsv(filename))
     indices_control_or_other = []
@@ -44,7 +46,7 @@ def data(filename, scale):
     for i in range(len(dataset)):
         if dataset[i][1] == 0 or dataset[i][1] == 9:
             indices_control_or_other.append(i)
-    
+
     dataset = np.delete(dataset, indices_control_or_other, 0) # delete rows that are Control/Other
     dataset = np.delete(dataset,[0, 11], 1) # deleting columns 0 and 11
 
@@ -66,7 +68,7 @@ def data(filename, scale):
 def evaluate(model_id, X, y, scale, seed=42):
     all_index = []
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
-    
+
     index_cap = len(y_train)
     index_10 = random.sample(range(index_cap),10)
     all_index.append(index_10)
@@ -89,61 +91,87 @@ def evaluate(model_id, X, y, scale, seed=42):
             break
         if (index_cap) > 1000:
             index_1000 = random.sample(range(index_cap),1000)
+            all_index.append(index_1000)
         else:
             index_1000 = random.sample(range(index_cap),index_cap)
             all_index.append(index_1000)
             break
         if (index_cap) > 2000:
             index_2000 = random.sample(range(index_cap),2000)
+            all_index.append(index_2000)
         else:
             index_2000 = random.sample(range(index_cap),index_cap)
             all_index.append(index_2000)
             break
 
-    
-    for i in all_index:
-    	length = len(i)
-    	print("Fitting model parameters on training set using", length, "rows of training set")
-    	t0 = time.time()
-    	grid = {'activation': ['identity','logistic', 'tanh', 'relu'], 'learning_rate': ['constant', 'invscaling', 'adaptive'],
-    	'hidden_layer_sizes': [(10, ), (20, ), (30, ), (40, ), (50, ), (10, 10, ), (20, 10, )], }
-    	clf = GridSearchCV(MLPRegressor(max_iter = 1000, solver = 'adam' , random_state=seed), param_grid=grid, cv=5, iid=False, scoring='neg_mean_squared_error')
-    	clf.fit(X_train[i], y_train[i])
-    	print("done in %0.3fs" % (time.time() - t0))
-    	print("\nBest estimator found by grid search:")
-    	print('\t'+str(clf.best_estimator_))
 
-    	print("\nEvaluating best estimator on test set")
-    	t0 = time.time()
-    	y_pred = clf.predict(X_test)
-    	print("done in %0.3fs" % (time.time() - t0))
-    		
-    	score = round(mean_absolute_error(y_test, y_pred), 4)
-    	print('\n\t\tMAE (test):', score)
+    for i, indices in enumerate(all_index):
+        length = len(indices)
+        print("Fitting model parameters on training set using", length, "rows of training set")
+        t0 = time.time()
+        grid = {'activation': ['identity','logistic', 'tanh', 'relu'], 'learning_rate': ['constant', 'invscaling', 'adaptive'],
+        'hidden_layer_sizes': [(10, ), (20, ), (30, ), (40, ), (50, ), (10, 10, ), (20, 10, )], }
+        clf = GridSearchCV(MLPRegressor(max_iter = 1000, solver = 'adam' , random_state=seed), param_grid=grid, cv=5, iid=False, scoring='neg_mean_squared_error')
+        clf.fit(X_train[indices], y_train[indices])
+        print("done in %0.3fs" % (time.time() - t0))
+        print("\nBest estimator found by grid search:")
+        print('\t'+str(clf.best_estimator_))
+
+        print("\nEvaluating best estimator on test set")
+        t0 = time.time()
+        y_pred = clf.predict(X_test)
+        print("done in %0.3fs" % (time.time() - t0))
+
+        score = round(mean_absolute_error(y_test, y_pred), 4)
+        print('\n\t\tMAE (test):', score)
+
+        scores[i].append(score)
 
 
     #Plot predicted vs. true twitch force per phase width
-    widths = np.unique(X_test[:,1])
-    plt.figure(figsize=(10, 15))
-    plt.suptitle('Amplitude vs. Twitch Force (blue = true, red = predicted)')
-    for w in range(len(widths)):
-        p = plt.subplot(math.ceil(len(widths)/2), 2, w+1)
-        axes = plt.gca()
-        axes.set_ylim(0, 1.1)
-        for i in range (len(y_test)):
-            if X_test[i,1] == widths[w]:
-                plt.plot([X_test[i,0], X_test[i,0]], [y_pred[i], y_test[i]], color='black', linestyle='dashed', zorder=1)
-                plt.scatter(X_test[i,0], y_test[i], marker='o', color='blue', zorder=2)
-                plt.scatter(X_test[i,0], y_pred[i], marker='X', color='red', zorder=2)
-        p.title.set_text('(phase width = '+str(widths[w])+')')
-    plt.subplots_adjust(hspace=.8)
-    plt.savefig("plots.png")
+    # widths = np.unique(X_test[:,1])
+    # plt.figure(figsize=(10, 15))
+    # plt.suptitle('Amplitude vs. Twitch Force (blue = true, red = predicted)')
+    # for w in range(len(widths)):
+    #     p = plt.subplot(math.ceil(len(widths)/2), 2, w+1)
+    #     axes = plt.gca()
+    #     axes.set_ylim(0, 1.1)
+    #     for i in range (len(y_test)):
+    #         if X_test[i,1] == widths[w]:
+    #             plt.plot([X_test[i,0], X_test[i,0]], [y_pred[i], y_test[i]], color='black', linestyle='dashed', zorder=1)
+    #             plt.scatter(X_test[i,0], y_test[i], marker='o', color='blue', zorder=2)
+    #             plt.scatter(X_test[i,0], y_pred[i], marker='X', color='red', zorder=2)
+    #     p.title.set_text('(phase width = '+str(widths[w])+')')
+    # plt.subplots_adjust(hspace=.8)
+    # plt.savefig("plots.png")
+
+
+scores_10 = []
+scores_20 = []
+scores_50 = []
+scores_100 = []
+scores_200 = []
+scores_500 = []
+scores_1000 = []
+scores_2000 = []
+
+scores = [scores_10, scores_20, scores_50, scores_100, scores_200, scores_500, scores_1000, scores_2000]
+experiment_strings = ["10", "20", "50", "100", "200", "500", "1000", "2000"]
+
+test_files = [] #corresponding order with scores
 
 # ### Methods to run:
 for filepath in glob.iglob(data_dir + '/*.csv'):
     X, y = data(filepath, True)
     print("Evaluating:" + filepath)
     for iteration in range(0, 20):
-    	print("Iteration:", iteration)
-    	evaluate(id, X, y, True, 42)
+        print("Iteration:", iteration)
+        evaluate(id, X, y, True, 42)
 
+
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+
+results_df = pd.DataFrame(scores)
+
+results_df.to_csv(f"MLP_pretrain_{current_time}.csv")

2feaa1eedc05e0fd6e5d2f2724ab1585ad60e1e8
danihaidar <31519318+danihaidar@users.noreply.github.com>
2020-05-20 17:35:14 -0300

    Add files via upload

--- MLP_active_learning.py
+++ MLP_active_learning.py
@@ -9,6 +9,7 @@ import sklearn
 from sklearn import preprocessing
 import glob
 from modAL.models import ActiveLearner
+from collections import defaultdict
 
 # Model Imports
 from sklearn.neural_network import MLPRegressor
@@ -21,12 +22,21 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.model_selection import train_test_split, GridSearchCV
 
 import matplotlib.pyplot as plt
-from modAL.models import ActiveLearner
-from modAL.uncertainty import entropy_sampling
-from modAL.uncertainty import uncertainty_sampling
+
+
+#### USER PARAMETERS: ####
+# Number of trials to run
+num_trials = 20
+
+# Number of rows for inital active learner training
+num_init_rows = 5
 
 # Directory with CSV files
 data_dir = "data"
+
+# Test ratio (for train-test split)
+test_ratio = 0.2
+#########################
 
 ### Load data from csv file
 def loadCsv(filename):
@@ -42,6 +52,7 @@ def data(filename, scale):
 
     #Load data from file
     dataset = np.array(loadCsv(filename))
+
     indices_control_or_other = []
 
     # find rows that are Control (0) or Other (9)
@@ -66,103 +77,90 @@ def data(filename, scale):
         X = preprocessing.scale(X)
     return X, y
 
-def MLP_regression_std(regressor, X):
-    _, std = regressor.predict(X,return_std=True)
-    query_idx = np.argmax(std)
-    return query_idx, X[query_idx]
-    _
+# def MLP_std_query(regressor, X):
+#     MLP = regressor.estimator
+#     per_layer_weight = np.array([i for i in MLP.coefs_])
+#     w_arr = np.std(per_layer_weight, axis=0)
+#     query_idx = np.argmax(w_arr)
+#     return query_idx, X[query_idx]
+
+
 ### Evaluate model
 def evaluate(model_id, X, y, scale=False, seed=42):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=seed)
     
-    index_cap = len(y_train)
-    # print("cap:" + str(index_cap))
-    
-    index_10 = random.sample(range(index_cap),10)
-    # if len(index_10) != len(set(index_10)):
-    #     print("Duplicates 10")
-    #     print(index_10)
-    index_20 = random.sample(range(index_cap),20)
-    # if len(index_20) != len(set(index_20)):
-    #     print("Duplicates 20")
-    #     print(index_20)
-    index_50 = random.sample(range(index_cap),50)
-    # if len(index_50) != len(set(index_50)):
-    #     print("Duplicates 50")
-    #     print(index_50)
-    index_100 = random.sample(range(index_cap),100)
-    # if len(index_100) != len(set(index_100)):
-    #     print("Duplicates 100")
-    #     print(index_100)
-    index_200 = random.sample(range(index_cap),200)
-    if (index_cap) > 500:
-        index_500 = random.sample(range(index_cap),500)
-    else:
-      index_500 = random.sample(range(index_cap),index_cap)
-    if (index_cap) > 1000:
-        index_1000 = random.sample(range(index_cap),1000)
-    else:
-      index_1000 = random.sample(range(index_cap),index_cap)
-    if (index_cap) > 2000:
-        index_2000 = random.sample(range(index_cap),2000)
-    else:
-      index_2000 = random.sample(range(index_cap),index_cap)
 
+    # choose a small amount of random rows for initial training (e.g. 5)
+    init_indices = np.random.choice(index_cap, num_init_rows, replace=False)
+    print(f"Indices of intial train data: {init_indices}")
+    init_X = X_train[init_indices]
+    init_y = y_train[init_indices]
+
+    # remove the initial rows from our train data
+    X_train = np.delete(X_train, init_indices, axis=0)
+    y_train = np.delete(y_train, init_indices, axis=0)
 
     
-    #all_index = [index_10,index_20,index_50,index_100,index_200,index_1000, index_2000, index_5000]
-    all_index = [index_10,index_20,index_50,index_100,index_200,index_500,index_1000,index_2000]
-    iteration_scores = []
-        
-    for i in all_index:
-        length = len(i)
-        #print("Fitting model parameters on training set using", length, "rows of training set" )
+    for num_train in experiments:
+        num_queries = num_train - num_init_rows
+
         t0 = time.time()
-        #grid = {'n_estimators': (10, 50, 100, 1000),'min_samples_split': [2,5,10]}
-        #clf = GridSearchCV(estimator = RandomForestRegressor(max_features = "auto", criterion = "mse", random_state = 42), param_grid=grid, cv=5, iid=False, scoring='neg_mean_squared_error')
-        n_initial = 10
-        clf = ActiveLearner(estimator = MLPRegressor(max_iter = 1000, solver='adam', random_state=seed),  query_strategy=uncertainty_sampling, X_training = X_train[i], y_training=y_train[i])
-        clf.teach(X_train[i], y_train[i])
-        #clf.fit(X_train[i], y_train[i])
-        #print("done in %0.3fs" % (time.time() - t0))
-        #print("\nBest estimator found by grid search:")
-        #print('\t'+str(clf.best_estimator_))
-    
-        #print("\nEvaluating best estimator on test set")
-        # t0 = time.time()
-        y_pred = clf.predict(X_test)
-        #print("done in %0.3fs" % (time.time() - t0))
-    
+        learner = ActiveLearner(estimator=MLPRegressor(max_iter = 1000, solver = 'adam' , random_state=seed), query_strategy=uncertainty, X_training=init_X, y_training=init_y)
+
+        for _ in range(num_queries):
+            query_idx, query_instance = learner.query(X_train)
+            learner.teach(X_train[query_idx].reshape(1, -1), np.array([y_train[query_idx]]))
+
+        y_pred = learner.predict(X_test)
+        print("done in %0.3fs" % (time.time() - t0))
+
         score = round(mean_absolute_error(y_test, y_pred), 4)
-        #print('\n\t\tMAE (test):', score)
-        iteration_scores.append(score)
-    print(iteration_scores)
-    
+        
+        # add score for this trial to results dictionary in correct experiment row
+        results[num_train].append(score)
 
+        print(f"MAE (test) with {num_train} rows: {score}")
 
-    #Plot predicted vs. true twitch force per phase width
-#    widths = np.unique(X_test[:,1])
-#    plt.figure(figsize=(10, 15))
-#    plt.suptitle('Amplitude vs. Twitch Force (blue = true, red = predicted)')
-#    for w in range(len(widths)):
-#        p = plt.subplot(math.ceil(len(widths)/2), 2, w+1)
-#        axes = plt.gca()
-#        axes.set_ylim(0, 1.1)
-#        for i in range (len(y_test)):
-#            if X_test[i,1] == widths[w]:
-#                plt.plot([X_test[i,0], X_test[i,0]], [y_pred[i], y_test[i]], color='black', linestyle='dashed', zorder=1)
-#                plt.scatter(X_test[i,0], y_test[i], marker='o', color='blue', zorder=2)
-#                plt.scatter(X_test[i,0], y_pred[i], marker='X', color='red', zorder=2)
-#        p.title.set_text('(phase width = '+str(widths[w])+')')
-#    plt.subplots_adjust(hspace=.8)
-#    plt.savefig("plots.png")
+def to_num(label):
+    # remove all non-numeric characters
+    label_str = ""
+    for char in label:
+        if char.isdigit():
+            label_str += char
+    return label_str
 
-# ### Methods to run:
+### Main Loop (Over Files)###
+print(f"Active Learning with MLP Regressor.")
+print(f"Model trained with {num_init_rows} rows before querying starts.")
 
 for filepath in glob.iglob(data_dir + '/*.csv'):
     X, y = data(filepath, True)
-    print("Evaluating:" + filepath)
-    for iteration in range(0, 20):
-        #print("Iteration:", iteration)
-        evaluate(id, X, y, True, 42)    
- # average through all files
+    
+    index_cap = int(len(X) * (1 - test_ratio))
+
+    # num of training rows
+    experiments = [10, 20, 50, 100, 200] 
+    
+    # add experiments for {500, 1000} if there are enough rows 
+    if index_cap > 500:
+        experiments.append(500)
+    if index_cap > 1000:
+        experiments.append(1000)
+
+    # always run an experiment with all data
+    experiments.append(index_cap)
+
+    # results dictionary with "number of training rows" as keys
+    results = defaultdict(list)
+
+    # RUN TRIALS
+    for trial in range(1, num_trials + 1):
+        print(f"FILE {filepath}. TRIAL {trial}/{num_trials}.")
+        evaluate(id, X, y, True, 42)
+
+    # convert to DataFrame for csv saving
+    results_df = pd.DataFrame.from_dict(results, orient="index")
+
+    results_df.to_csv(f"MLP_ActiveLearn_{to_num(filepath)}.csv")
+
+
